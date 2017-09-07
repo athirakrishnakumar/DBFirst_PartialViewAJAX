92f976fa7166d14b9748bc73155e0a9f5b485515
akrishnakumar <akrishnakumar@SUYPC227>
2017-09-07 16:42:41 +0530

    Add project files.

---
+++ MVCDBFirst/Controllers/MyActionController.cs
@@ -0,0 +1,96 @@
+﻿using MVCDBFirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDBFirst.Controllers
+{
+    public class MyActionController : Controller
+    {
+        AthiraEntities dbContext = new AthiraEntities();
+        // GET: MyAction
+        public ActionResult Index()
+        {
+            List<Detail> person = dbContext.Details.ToList();
+            return View(person);
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public ActionResult Add(Detail person)
+        {
+            Detail det = dbContext.Details.Where(c => c.id == person.id).FirstOrDefault();
+            dbContext.Details.Add(person);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Detail det = dbContext.Details.Where(c => c.id == id).FirstOrDefault();
+            return View(det);
+        }
+
+        [HttpPost]
+        
+        public ActionResult Edit(Detail person)
+        {
+            Detail det = dbContext.Details.Where(c => c.id == person.id).FirstOrDefault();
+            dbContext.Entry(det).CurrentValues.SetValues(person);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Detail det = dbContext.Details.Where(c => c.id == id).FirstOrDefault();
+            return View(det);
+        }
+
+        [HttpPost]
+
+        public ActionResult Delete(Detail person)
+        {
+            Detail der = dbContext.Details.Where(c => c.id == person.id).FirstOrDefault();
+            dbContext.Details.Remove(der);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //public ActionResult Details(int id)
+        //{
+        //    Detail det = dbContext.Details.Where(c => c.id == id).FirstOrDefault();
+        //    return RedirectToAction("Index");
+        //}
+
+        //[HttpPost]
+
+        public ActionResult Search()
+        {
+            //Detail det = dbContext.Details.Where(c => c.id == person.id).FirstOrDefault();
+            return View();
+        }
+
+        public ActionResult SearchPartialView(int id)
+        {
+            Detail det = dbContext.Details.Where(c => c.id == id).FirstOrDefault();
+            return PartialView(det);
+        }
+
+
+        public ActionResult Details(int id)
+        {
+            AthiraEntities dbContext = new AthiraEntities();
+            Detail person = dbContext.Details.Where(c => c.id == id).FirstOrDefault();
+            return View(person);
+        }
+
+    }
+}
---
+++ MVCDBFirst/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCDBFirst.Startup))]
+namespace MVCDBFirst
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
